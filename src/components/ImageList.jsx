24602ddee22dfeb9e97351d44684a020534fea62
Jetigeri <szabo.gergo87j@gmail.com>
2022-03-13 11:16:53 +0100

    Lesson: refs

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,10 +1,13 @@
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
+
 const ImageList = (props) => {
   console.log(props.images);
-  const images = props.images.map(({alt_description, id, urls}) => (
-    <img key={id} src={urls.regular} alt={alt_description}></img>
-  ))
+  const images = props.images.map((image) => (
+    <ImageCard key={image.id} image={image}></ImageCard>
+  ));
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
